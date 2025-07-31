5da19aa917af9ad429401cb024cfdbb97d5032a6
SSworup77 <98733444+SSworup77@users.noreply.github.com>
2025-07-31 23:15:15 +0545

    Added newer dependencies and configs

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 import Image from "next/image";
 
 export default function Home() {
-	return <div>Hello</div>;
+  return (
+    <div className="flex min-h-screen items-start justify-start">
+      <h1>Hello There!</h1>
+      <p>
+        I am a eager learning passionate problem solver who loves to build
+        things with code. I am currently learning Next.js and exploring its
+        capabilities.
+      </p>
+    </div>
+  );
 }
