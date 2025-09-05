d30d5247d5207fd8490a457e2af4f74ec844335d
SSworup77 <98733444+SSworup77@users.noreply.github.com>
2025-09-05 22:22:22 +0545

    Changes for footer misalignment

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='flex mt-20 items-center justify-center pt-4 max-w-3xl '>
+    <footer className='flex mt-20 items-center justify-center pt-4 max-w-4xl '>
         <p className='text-secondary'>© 2025 Made with ❤️ by Sworup Shrestha. All rights reserved.</p>
     </footer>
   )
